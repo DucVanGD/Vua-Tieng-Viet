c792a7b7e0f634d104e0d32e8939699007bc37a3
DucVanGD <pducvv09@gmail.com>
2025-09-28 11:28:03 +0700

    sample

---
+++ README.md
@@ -0,0 +1,2 @@
+Tài liệu hướng dẫn dự án: 
+https://docs.google.com/document/d/1elJ2cUsU1nXjL1C9LeGvvJGnBQM_ca7-3A6jQZUBzCk/edit?tab=t.0#heading=h.52a18iyl1ydd
---
+++ src/main/java/com/mycompany/vua_tieng_viet/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.vua_tieng_viet;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
